34236c5f11fc3a0a668d2ac7cc9259ed21c3f148
Emmanuel Monsalve <emonsalvep38@gmail.com>
2022-05-31 10:37:11 -0500

    PUSH ALL CHANGES

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 import MainPage from './pages/MainPage';
-import './App.css';
+import Encuesta from './pages/Encuesta';
+import Nabvar from './components/Nabvar';
+
+import { Container } from '@mui/material';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <MainPage />
+      <BrowserRouter>
+        <Nabvar />
+        <Container>
+          <Routes>
+            <Route index path="/" exact element={<MainPage />} />
+            <Route path="/encuesta" element={<Encuesta />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </div>
   );
 };
--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Todo = require('../models/todo');
 
-router.get('/todos', (req, res, next) => {
-    // This will return all the data, exposing only the id and action field to the client
-    Todo.find({}, 'action')
-        .then((data) => res.json(data))
-        .catch(next);
-});
-
 router.post('/todos', (req, res, next) => {
     if (req.body.action) {
         Todo.create(req.body)
@@ -21,10 +14,5 @@ router.post('/todos', (req, res, next) => {
     }
 });
 
-router.delete('/todos/:id', (req, res, next) => {
-    Todo.findOneAndDelete({ _id: req.params.id })
-        .then((data) => res.json(data))
-        .catch(next);
-});
 
 module.exports = router;
